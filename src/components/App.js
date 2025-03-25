b2b88ac0c8d7afc7ee1bb01b1e1ad6c962d7b520
Nishar24 <mdnishar75640@gmail.com>
2025-03-25 23:58:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,30 @@
-import React, { useState, useMemo } from "react";
-import ReactMemoComponent from "./ReactMemoComponent";
-import UseMemoComponent from "./UseMemoComponent";
+import React, { useState } from "react";
+import UseMemo from "./UseMemo";
+import ReactMemo from "./ReactMemo";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [counter, setCounter] = useState(0);
-  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-  const addTodo = () => {
-    setTodos([...todos, "New todo"]);
+  const onAdd = () => {
+    setTasks((prev) => [...prev, "New Todo"]);
   };
-
-  const addCustomTodo = () => {
-    if (input.length > 5) {
-      setTodos([...todos, input]);
-      setInput("");
-    } else {
-      alert("Task must be more than 5 characters!");
-    }
-  };
-
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const memoizedTodos = useMemo(() => todos, [todos]);
-
   return (
-    <div>
-      <h1>React Performance Optimization</h1>
-      <button onClick={addTodo}>Add Todo</button>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Enter task"
-      />
-      <button onClick={addCustomTodo}>Submit</button>
+    <div id="main">
+      <h1>React.useMemo</h1>
+      <h2>My todos</h2>
       <ul>
-        {memoizedTodos.map((todo, index) => (
-          <ReactMemoComponent key={index} todo={todo} />
+        {tasks.map((i, index) => (
+          <li key={index}>{i}</li>
         ))}
       </ul>
-      <button onClick={incrementCounter}>Increment Counter</button>
-      <p>Counter: {counter}</p>
-      <UseMemoComponent counter={counter} />
+
+      <button onClick={onAdd}>Add Todo</button>
+
+      <hr />
+      <UseMemo />
+      <hr />
+      <hr />
+      <ReactMemo />
     </div>
   );
 };
